f4154b4c42474c3181b16afdc780e1b41e9083a9
GoldenPlox <edwin.arroyo@ucuenca.edu.ec>
2023-04-04 20:03:17 -0500

    Fisrt commint

---
+++ Ejercisio 1.py
@@ -0,0 +1,16 @@
+print("Fundamentos de Programacion")
+print("Nombre: Edwin Arroyo")
+print("Fecha: 04/04/2023")
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+iguales = num1 == num2
+diferentes = num1 != num2
+mayor = num1 > num2
+mayorigual = num2 >= num1
+
+print("Son iguales: ", iguales)
+print("Son diferentes: ", diferentes)
+print("El primero es mayor que el segundo: ", mayor)
+print("El segundo es mayor o igual que el primero: ", mayorigual)
---
+++ Ejercisio 2.py
@@ -0,0 +1,6 @@
+print("Fundamentos de Programacion")
+print("Nombre: Edwin Arroyo")
+print("Fecha: 04/04/2023")
+
+nombre = input("Ingrese su nombre: ")
+print("Hola, " + nombre + " Bienvenido.")
